d820f196ff26a793013739d0b4901cddd0a52f90
Jesse R Reeves <143226100+JesseRReeves@users.noreply.github.com>
2023-10-04 23:04:18 -0700

    Saving

--- pybank/main.py
+++ pybank/main.py
@@ -1,26 +1,44 @@
+# Import operating system
 import os
+
+# Import module for reading csv files
 import csv
 
-# Set path to collect data from the resources folder.
+# Set path to collect data from the resources folder
 pybank_csv = os.path.join("Resources", "budget_data.csv")
 
-#Open and read csv
+ # Define total_months
+total_months = 0
+
+# Define net Total profits/losses
+total_net = 0
+
+# Open and read csv
 with open(pybank_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     
-    #Read the header row
+    # Read the header row
     csv_header = next(csv_reader)
-    print(f"Header {csv_header}")
+    
+
+    first_row = next(csv_reader)
    
-    # Define total_months
-    total_months = 0
+    # Track the total months
+    total_months += 1
+    
+
    
-    #Read through each row of data after the header
+    # Read through each row of data after the header
     for row in csv_reader:
 
-        #Track the total
+        # Track the total months
         total_months += 1
-        print(total_months)
+        total_net += int(row[1])
+    print(total_net)
+
+    
+        
+     
 
 
 
